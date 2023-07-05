1238797dd96adc0331bb727d2545fabb3e238e82
galim-kang <132195884+galim-kang@users.noreply.github.com>
2023-07-05 15:04:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> 가림님 대 성공
         </p>
         <a
           className="App-link"
